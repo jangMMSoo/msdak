c5271c424d3fbe3c99ff54e42a138096ea5d7fb7
msdak <ntaro117@gmail.com>
2019-11-04 23:04:04 +0900

    file read add

--- today_on_eat_to_what/src/main.rs
+++ today_on_eat_to_what/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("../today_on_eat_to_what/food_data.json")
+        .expect("Somthing went wron..read file");
+
+    println!("with text \n{}", contents);
 }
