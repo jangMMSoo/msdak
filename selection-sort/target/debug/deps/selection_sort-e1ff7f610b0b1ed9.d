21002bf0f6de2dfe89f304b3aa6c0f519c7e7104
msdak <ntaro117@gmail.com>
2019-11-21 22:32:44 +0900

    ttt

---
+++ selection-sort/target/debug/deps/selection_sort-e1ff7f610b0b1ed9.d
@@ -0,0 +1,5 @@
+/Users/meongsoojang/RustProject/msdak/selection-sort/target/debug/deps/selection_sort-e1ff7f610b0b1ed9: src/main.rs
+
+/Users/meongsoojang/RustProject/msdak/selection-sort/target/debug/deps/selection_sort-e1ff7f610b0b1ed9.d: src/main.rs
+
+src/main.rs:
