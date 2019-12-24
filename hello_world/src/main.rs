a2bac93c0ecb2e0a6278a3c2b83894b372f05c2f
msdak <ntaro117@gmail.com>
2019-12-24 09:05:36 +0900

    ssh test

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -49,5 +49,19 @@ fn main() {
         println!("{}!", n);
     }
 
+    let x = 5;
+    let y = x;
 
+    println!("x is {}", x);
+    println!("y is {}", y);
+
+    
+    let s1 = String::from("hello");
+    let s2 = s1; //<- s1의 포인터를 s2 로 넘겨줌, 오너쉽을 넘김
+    //println!("s1 is {}", s1);
+    println!("s2 is {}", s2);
+
+    let s3 = s2.clone(); //비ㅆ비다
+    println!("s2 is {}, s3 is {}", s2, s3);
+    
 }
