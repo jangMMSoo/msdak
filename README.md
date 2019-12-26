e6df6b7c09e2ba7f2f00d9c3f2dd99cef672e552
mengsoo <ntaro117@gmail.com>
2019-12-27 05:40:58 +0900

    슬라이스 예제 추가

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 https://ntaro117.visualstudio.com/learn-rust/_wiki/wikis/learn-rust.wiki?wikiVersion=GBwikiMaster
 
 https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html
+
+https://doc.rust-lang.org/book/ch04-03-slices.html
--- dangling/src/main.rs
+++ dangling/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let ref_to_nothing = dangle();
+
+    println!("{}", ref_to_nothing);
+
+    let s = String::from("hello world!");
+    let word = first_word(&s);
+    println!("{}", word);
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+    s
+}
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
